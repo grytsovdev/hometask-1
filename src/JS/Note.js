52800778aa24904d0872821d9aea9d530933a55a
Ian <grytsov.ian@gmail.com>
2023-07-29 15:29:02 +0300

    Implement note management feature

--- src/JS/Note.js
+++ src/JS/Note.js
@@ -1,17 +1,30 @@
 export default class Note {
-  constructor(id, title, body, category, mentionedDates) {
+  constructor(id, title, body, category, createdDate) {
     this.id = id;
     this.title = title;
     this.body = body;
     this.category = category;
-    this.createdDate = new Date();
-    this.mentionedDates = mentionedDates;
+    if (!createdDate) this.createdDate = this.getCreatedDate();
+    else this.createdDate = createdDate;
+    this.mentionedDates = this.getMentionedDates(body);
     this.archived = false;
   }
+
   archive() {
     this.archived = true;
   }
   unarchive() {
     this.archived = false;
+  }
+
+  getCreatedDate() {
+    const date = new Date();
+    return date.toLocaleDateString("en-GB");
+  }
+
+  getMentionedDates(body) {
+    const dateRegex = /\d{2}([\/.-])\d{2}\1\d{4}/gi;
+    const dates = body.match(dateRegex);
+    return dates ? dates : "";
   }
 }
