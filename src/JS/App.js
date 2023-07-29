b7cf4c5f7f33edb6b31183bb106aa0a09c52e346
Ian <grytsov.ian@gmail.com>
2023-07-29 15:29:02 +0300

    Implement note management feature

--- src/JS/App.js
+++ src/JS/App.js
@@ -1,16 +1,168 @@
-import { notes } from "./NotesData";
+import {
+  addNote,
+  archiveNote,
+  archivedNotes,
+  deleteAllNotes,
+  deleteNote,
+  editNote,
+  notes,
+  unarchiveNote,
+} from "./NotesData";
 const notesContainer = document.querySelector(".notes-container");
+const modalContainer = document.querySelector(".modal-container");
+const addNoteButton = document.querySelector(".add-note-button");
+const modalCancelButton = document.querySelector(".modal-cancel");
+const modalSubmitButton = document.querySelector(".modal-submit");
+const showArchivedNotesButton = document.querySelector(".archived-button");
+const deleteAllButton = document.querySelector(".delete-all-button");
+const appHeader = document.querySelector(".app-header");
 
-function renderNotesList() {
+let showArchived = false;
+
+function renderNotesList(notes) {
+  const notesHeader = document.querySelector(".notes-header");
+  notesContainer.innerHTML = "";
+  notesContainer.appendChild(notesHeader);
+
   notes.forEach((note) => {
     const newUINote = document.createElement("div");
     newUINote.classList.add("note");
-    newUINote.innerHTML = ` <div>${note.title}</div>
+
+    const deleteNoteButton = document.createElement("button");
+    deleteNoteButton.classList.add("delete-note-button");
+    deleteNoteButton.setAttribute("data-note-id", note.id);
+
+    const archiveNoteButton = document.createElement("button");
+    archiveNoteButton.classList.add("archive-note-button");
+    archiveNoteButton.setAttribute("data-note-id", note.id);
+    archiveNoteButton.setAttribute("data-archived", note.archived);
+
+    const editNoteButton = document.createElement("button");
+    editNoteButton.classList.add("edit-note-button");
+    editNoteButton.setAttribute("data-note-id", note.id);
+
+    let categoryIconClass = "";
+    switch (note.category) {
+      case "Task":
+        categoryIconClass = "tasks";
+        break;
+      case "Idea":
+        categoryIconClass = "lightbulb";
+        break;
+      case "Random Thought":
+        categoryIconClass = "comment";
+        break;
+    }
+
+    newUINote.innerHTML = ` <div><i class="${categoryIconClass}"></i>${note.title}</div>
     <div>${note.createdDate}</div>
     <div>${note.category}</div>
     <div>${note.body}</div>
-    <div>${note.mentionedDates}</div>`;
+    <div>${note.mentionedDates}</div>
+    <div class="controls-container" />`;
+
+    newUINote.querySelector(".controls-container").appendChild(editNoteButton);
+
+    newUINote
+      .querySelector(".controls-container")
+      .appendChild(archiveNoteButton);
+
+    newUINote
+      .querySelector(".controls-container")
+      .appendChild(deleteNoteButton);
+
     notesContainer.appendChild(newUINote);
   });
 }
-renderNotesList();
+
+notesContainer.addEventListener("click", (event) => {
+  const target = event.target;
+  const noteId = parseInt(target.getAttribute("data-note-id"));
+  if (target.classList.contains("delete-note-button")) {
+    deleteNote(noteId);
+    renderNotesList(notes);
+  } else if (target.classList.contains("archive-note-button")) {
+    const isArchived = target.getAttribute("data-archived") === "true";
+    if (isArchived) {
+      unarchiveNote(noteId);
+    } else {
+      archiveNote(noteId);
+    }
+    renderNotesList(showArchived ? archivedNotes : notes);
+  } else if (target.classList.contains("edit-note-button")) {
+    openEditModal(noteId);
+  }
+});
+
+addNoteButton.addEventListener("click", () => {
+  modalContainer.classList.add("active");
+});
+
+modalCancelButton.addEventListener("click", () => {
+  modalContainer.classList.remove("active");
+});
+
+showArchivedNotesButton.addEventListener("click", () => {
+  showArchived = !showArchived;
+  appHeader.innerText = showArchived ? "Archive" : "Your notes";
+  const notesArray = showArchived ? archivedNotes : notes;
+  renderNotesList(notesArray);
+});
+
+deleteAllButton.addEventListener("click", () => {
+  deleteAllNotes();
+  renderNotesList(notes);
+});
+
+modalSubmitButton.addEventListener("click", addNewUINote);
+
+function addNewUINote() {
+  const title = document.querySelector("#title");
+  const category = document.querySelector("#category");
+  const content = document.querySelector("#content");
+
+  if (
+    title.value.trim().length > 0 &&
+    content.value.trim().length > 0 &&
+    category.value.trim().length > 0
+  ) {
+    addNote(title.value, content.value, category.value);
+    renderNotesList(notes);
+    modalContainer.classList.remove("active");
+    title.value = "";
+    content.value = "";
+    category.value = "";
+  }
+}
+
+function openEditModal(id) {
+  const note = notes.find((note) => note.id === id);
+  const title = document.querySelector("#title");
+  const category = document.querySelector("#category");
+  const content = document.querySelector("#content");
+
+  title.value = note.title;
+  category.value = note.category;
+  content.value = note.body;
+
+  modalSubmitButton.innerText = "Save note";
+  modalSubmitButton.removeEventListener("click", addNewUINote);
+  modalSubmitButton.addEventListener("click", () => {
+    saveEditedNote(id);
+  });
+
+  modalContainer.classList.add("active");
+}
+
+function saveEditedNote(id) {
+  const title = document.querySelector("#title");
+  const category = document.querySelector("#category");
+  const content = document.querySelector("#content");
+
+  editNote(id, title.value, content.value, category.value);
+
+  renderNotesList(notes);
+  modalContainer.classList.remove("active");
+}
+
+renderNotesList(notes);
--- src/JS/NotesData.js
+++ src/JS/NotesData.js
@@ -3,69 +3,122 @@ const notes = [
   new Note(
     1,
     "Dentist Appointment",
-    "I'm gonna have a dentist appointment on the 3/5/2021, I moved it from 5/5/2021",
+    "I'm gonna have a dentist appointment on the 03/05/2021, I moved it from 05/05/2021",
     "Task",
-    ["3/5/2021", "5/5/2021"]
+    "21/07/2023"
   ),
   new Note(
     2,
     "Grocery Shopping",
     "Remember to buy groceries today",
     "Task",
-    []
+
+    "18/07/2023"
   ),
   new Note(
     3,
     "New Project Idea",
     "Had a great idea for a new project",
     "Idea",
-    []
+    "12/07/2023"
   ),
-  new Note(4, "Call John", "I need to call John tomorrow", "Task", []),
+  new Note(
+    4,
+    "Call John",
+    "I need to call John tomorrow",
+    "Task",
+    "25/06/2023"
+  ),
   new Note(
     5,
     "Try New Restaurant",
     "What if we try the new restaurant on Friday?",
     "Idea",
-    []
+    "06/07/2023"
   ),
   new Note(
     6,
     "Upcoming Trip",
     "Feeling excited about the upcoming trip",
     "Random Thought",
-    []
+    "01/07/2023"
   ),
   new Note(
     7,
     "Presentation Deadline",
-    "Need to finish the presentation by 10/8/2021",
+    "Need to finish the presentation by 10/08/2021",
     "Task",
-    ["10/8/2021"]
+    "29/06/2023"
   ),
 ];
 
+const archivedNotes = [];
+
+function sortNotes(notes) {
+  notes.sort((a, b) => {
+    const dateA = a.createdDate.split("/").reverse().join("");
+    const dateB = b.createdDate.split("/").reverse().join("");
+    return dateB.localeCompare(dateA);
+  });
+}
+
+sortNotes(notes);
+
 function addNote(title, content, category) {
   const id = notes.length + 1;
-  const mentionedDates = getMentionedDates(content);
-  const newNote = new Note(id, title, content, category, mentionedDates);
+  const newNote = new Note(id, title, content, category);
+
   notes.push(newNote);
+  sortNotes(notes);
 }
 
 function deleteNote(id) {
-  notes.slice(
-    notes.findIndex((note) => {
-      note.id === id;
-    }, 1)
-  );
+  const index = notes.findIndex((note) => note.id === id);
+  if (index !== -1) {
+    notes.splice(index, 1);
+  }
 }
 
-function editNote() {
-  //implement edeting of the notes
+function deleteAllNotes() {
+  notes.length = 0;
 }
 
-function getMentionedDates(content) {
-  // implement extraction of mentiond dates
+function editNote(id, title, content, category) {
+  console.log(title);
+  const index = notes.findIndex((note) => note.id === id);
+  notes[index].title = title;
+  notes[index].content = content;
+  notes[index].category = category;
 }
 
-export { addNote, deleteNote, editNote, notes };
+function archiveNote(id) {
+  const index = notes.findIndex((note) => note.id === id);
+  if (index !== -1) {
+    notes[index].archive();
+    archivedNotes.push(notes[index]);
+    notes.splice(index, 1);
+    sortNotes(archivedNotes);
+  }
+}
+
+function unarchiveNote(id) {
+  const index = archivedNotes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    archivedNotes[index].unarchive();
+    notes.push(archivedNotes[index]);
+    archivedNotes.splice(index, 1);
+    sortNotes(notes);
+  }
+}
+
+export {
+  addNote,
+  archiveNote,
+  archivedNotes,
+  deleteAllNotes,
+  deleteNote,
+  editNote,
+  notes,
+  unarchiveNote,
+};
